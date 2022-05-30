a61bdf49f611f5a7eba11e9b97785986e8d1e4ce
Jean Simas <jeansimas12@hotmail.com>
2022-05-30 13:31:00 -0300

    Finished Multiplication Table

--- main.go
+++ main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+func generateMultTable() {
+	for i := 1; i < 11; i++ {
+		for j := 1; j < 11; j++ {
+			fmt.Println(fmt.Sprintf("%v x %v = %v", i, j, i*j))
+		}
+		fmt.Println()
+	}
+}
+
 func main() {
-	fmt.Println("Teste")
+	generateMultTable()
 }
